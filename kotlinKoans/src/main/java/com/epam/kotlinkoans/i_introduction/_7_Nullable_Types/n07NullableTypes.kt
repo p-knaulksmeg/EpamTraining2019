87c049e3101265e67fc9b6da478e271f2448413f
Paul <pavelkeytof@gmail.com>
2019-04-04 23:41:33 +0300

    done

--- kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -26,7 +26,10 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
     client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    val email: String = client?.personalInfo?.email ?: return
+    if (message != null) {
+        mailer.sendMessage(email, message)
+    }
 }
 
 class Client(val personalInfo: PersonalInfo?)
--- kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/MyDate.kt
+++ kotlinKoans/src/main/java/com/epam/kotlinkoans/iii_conventions/MyDate.kt
@@ -1,8 +1,13 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
-
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    // #25
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
 enum class TimeInterval {
     DAY,
@@ -11,3 +16,34 @@ enum class TimeInterval {
 }
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+// #26
+operator fun DateRange.contains(date: MyDate) = date >= start && date <= endInclusive
+
+// #27
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+// #28
+operator fun DateRange.iterator(): Iterator<MyDate> =
+        object : Iterator<MyDate> {
+            var current = start
+
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                return current.apply {
+                    current = current.nextDay()
+                }
+            }
+        }
+
+// #29
+operator fun MyDate.plus(interval: TimeInterval) : MyDate = addTimeIntervals(interval, 1)
+
+data class RepeatedTimeInterval(val ti: TimeInterval, val n: Int)
+
+operator fun TimeInterval.times(n: Int): RepeatedTimeInterval = RepeatedTimeInterval(this, n)
+
+operator fun MyDate.plus(interval: RepeatedTimeInterval) : MyDate = addTimeIntervals(interval.ti, interval.n)
